f1a3719558213661e1aca61d6d0e5f4fedc79c1e
yoppieariesthio <102799135+yoppieariesthio@users.noreply.github.com>
2025-08-04 18:25:12 +0800

    Add files via upload

---
+++ controller/Profile.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/core/format/DateFormat"
+], function (Controller, MessageToast, DateFormat) {
+    "use strict";
+
+    return Controller.extend("yoppieApp.controller.Profile", {
+        onInit: function () {
+        //     // Initialize controller
+        //     this._oRouter = this.getOwnerComponent().getRouter();
+        //     this._oModel = this.getOwnerComponent().getModel("profile");
+        },
+
+     });
+});
