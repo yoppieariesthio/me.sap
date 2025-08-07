5428a3f4f7f8779ddf9827ddc6328cb9dec7cc82
yoppieariesthio <102799135+yoppieariesthio@users.noreply.github.com>
2025-08-07 11:27:24 +0800

    clean

--- Component.js
+++ Component.js
@@ -10,14 +10,7 @@ sap.ui.define([
       init : function () {
          // call the init function of the parent
          UIComponent.prototype.init.apply(this, arguments);
-         // set data model
-        //  var oData = {
-        //     recipient : {
-        //        name : "World"
-        //     }
-        //  };
-        //  var oModel = new JSONModel(oData);
-        //  this.setModel(oModel);
+
       }
    });
 });
